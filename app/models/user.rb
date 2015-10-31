456ba98d888718ee3e0298cd7db46e718a0f1a22
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-11-01 00:57:55 +0900

    ユーザ名でログインできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:twitter]
   has_many :articles
+  validates :username, presence: true, uniqueness: true
 
   def self.from_omniauth(auth)
     where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
