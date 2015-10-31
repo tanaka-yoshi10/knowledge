0abf6be3285b88bd0ac17fcebfa4d3e157c81133
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-10-31 17:14:28 +0900

    不要な処理を削除

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,4 @@ class PagesController < ApplicationController
 
   def index
   end
-
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'pages#index'
 
-  get 'pages/show'
-
   resources :articles
 end
