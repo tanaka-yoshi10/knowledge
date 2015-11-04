b38a4fa9f37cb484314d209225cfdf543a0b83f4
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-11-04 22:59:20 +0900

    ストック記事一覧表示

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
 
   def index
     @articles = Article.all.includes(:user) # TODO: 公開のみにする
+    @stocked_articles = Article.stocked_by(current_user).includes(:user)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :stocks
+
+  # scopeにすべき？
+  def self.stocked_by(user)
+    stocked_article_ids = "SELECT article_id FROM stocks WHERE user_id = :user_id"
+    where("articles.id IN (#{stocked_article_ids})", user_id: user.id)
+  end
 end
