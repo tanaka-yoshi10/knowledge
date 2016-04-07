104236cbeb0cf519c3b786b9290b66102deba9a4
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-04-08 07:42:33 +0900

    add spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a title and body' do
+    user = create(:user)
+    article = build(:article, author: user)
+    expect(article).to be_valid
+  end
+
+  it 'is invalid without a title' do
+    user = create(:user)
+    article = build(:article, title: nil, author: user)
+    expect(article).to be_invalid
+  end
 end
