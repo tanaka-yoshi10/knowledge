bbea627004b3ed6d6b113d22513c1f26f3e6aed8
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-04-12 08:07:49 +0900

    公開済みのみ一覧に表示する

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @articles = Article.all.includes(:author) # TODO: 公開のみにする
+    @articles = Article.published.includes(:author)
     @stocking_articles = current_user.stocking_articles.includes(:author)
   end
 end
--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Pages' do
   scenario 'show top page' do
     user = create(:user)
-    article = create(:article, author: user)
+    article = create(:article, author: user, status: :published)
 
     visit root_path
     fill_in '名前', with: user.name
@@ -12,5 +12,18 @@ feature 'Pages' do
 
     visit root_path
     expect(page).to have_content 'MyString'
+  end
+
+  scenario '下書きは一覧に表示されない' do
+    user = create(:user)
+    article = create(:article, author: user, status: :draft)
+
+    visit root_path
+    fill_in '名前', with: user.name
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    visit root_path
+    expect(page).not_to have_content 'MyString'
   end
 end
