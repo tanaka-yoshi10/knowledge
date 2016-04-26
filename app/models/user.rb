23cbeb298fc177cd064bc44b3377444d35ee345d
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-04-27 08:45:39 +0900

    dependency追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:twitter]
   has_many :articles, foreign_key: :author_id
-  has_many :stocks
+  has_many :stocks, dependent: :destroy
   has_many :relationships, foreign_key: :follower_id, dependent: :destroy
   has_many :reverse_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   # TODO 上の文字列はシンボルにできないのか？
