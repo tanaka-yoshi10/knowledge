4fdfe1bcbec7e9c824e6c2401a443557fd818eb7
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-05-03 07:17:01 +0900

    タグ一覧に下書きを含まないようにする

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.most_used
+    @tags = Tag.most_used_in_published
   end
 
   def show
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,14 @@
 class Tag < ActiveRecord::Base
   has_many :tagfollows
   has_many :users, through: :tagfollows
-end
 
+  # TODO: acts-as-taggable-onをうまく活用できなかったため、SQLを直接発行して実現している。見直したい。
+  scope :most_used_in_published, -> {
+    joins('INNER JOIN taggings ON tags.id = taggings.tag_id')
+      .joins('INNER JOIN articles ON taggings.taggable_id = articles.id AND taggings.taggable_type = "Article"')
+      .group(:id)
+      .order('taggings_count DESC')
+      .where('articles.status = ?', Article.statuses[:published])
+      .select('tags.id, tags.name, COUNT(tags.id) as taggings_count')
+  }
+end
