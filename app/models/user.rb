45ca86ef079fbb0c3f4642968ae234789f32e6b5
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-06-19 14:23:45 +0900

    find_or_create_byでコードを読みやすくする

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   def self.from_omniauth(auth)
-    where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
+    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
       user.name = auth.info.nickname
       user.email = auth.info.email
     end
