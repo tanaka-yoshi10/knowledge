75163542b745210a744a77dfc76b95e474a9c63e
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-06-19 09:06:50 +0900

    書き方を工夫し、コードをすっきりさせる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :email]
     devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
