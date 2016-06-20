dbeb597c179493cece1e6ff741f5014028e7d9b9
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-06-21 08:00:39 +0900

    let, subjectを使ってみる

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,30 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   describe "#full_name" do
-    context "両方ある場合" do
-      it "間にスペースが入ること" do
-        profile = create(:profile, first_name: "First", last_name: "Last")
-        expect(profile.full_name).to eq "First Last"
+    subject { profile.full_name }
+
+    context "姓名両方ある場合" do
+      let(:profile) { create(:profile, first_name: "First", last_name: "Last") }
+      it "間にスペースか入ること" do
+        is_expected.to eq "First Last"
       end
     end
 
     context "名のみの場合" do
+      let(:profile) { create(:profile, first_name: "First") }
       it "スペースが入らないこと" do
-        profile = create(:profile, first_name: "First")
-        expect(profile.full_name).to eq "First"
+        is_expected.to eq "First"
       end
     end
 
     context "姓のみの場合" do
+      let(:profile) { create(:profile, last_name: "Last") }
       it "スペースが入らないこと" do
-        profile = create(:profile, last_name: "Last")
-        expect(profile.full_name).to eq "Last"
+        is_expected.to eq "Last"
       end
     end
 
     context "両方空の場合" do
+      let(:profile) { create(:profile) }
       it "空になること" do
-        profile = create(:profile)
         expect(profile.full_name.blank?).to be_truthy
       end
     end
